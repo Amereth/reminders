6221e936e0cdd1d0ed2131a2867641eb514b9c1a
Oleksandr-Melnyk-Artelogic <omelnyk@artelogic.net>
2024-03-02 21:32:07 +0200

    db and env setup

---
+++ src/utils/env.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+const envSchema = z.object({
+  DIRECT_URL: z.string(),
+  DATABASE_URL: z.string(),
+  TELEGRAM_BOT_TOKEN: z.string(),
+});
+
+export const env = envSchema.parse(process.env);
