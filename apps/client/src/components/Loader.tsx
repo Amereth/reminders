31dfc82ec9cefaf7e256326dffea454e40a856e4
Amereth <alek.melnyk@gmail.com>
2024-04-07 17:23:04 +0300

    refactor: added optional size prop to loader

--- apps/client/src/components/Loader.tsx
+++ apps/client/src/components/Loader.tsx
@@ -1,7 +1,13 @@
+import { cn } from '@/lib/utils'
 import { LoaderCircleIcon } from 'lucide-react'
 
-export const Loader = () => (
-  <div className='grid h-full place-content-center'>
-    <LoaderCircleIcon size={36} className='animate-spin' />
+type LoaderProps = {
+  size?: number | string
+  className?: string
+}
+
+export const Loader = ({ size = 36, className }: LoaderProps) => (
+  <div className={cn('grid h-full place-content-center', className)}>
+    <LoaderCircleIcon size={size} className='animate-spin' />
   </div>
 )
