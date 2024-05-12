400d627df552a1dca97832aa5e138c9b035f463e
Amereth <alek.melnyk@gmail.com>
2024-05-12 23:46:44 +0300

    feature: load more button

--- apps/client/src/components/events/event-list.tsx
+++ apps/client/src/components/events/event-list.tsx
@@ -1,12 +1,16 @@
 import { useDeleteEventMutation } from '@/hooks/api/events'
 import { EventCard } from './EventCard'
 import { Event } from '@reminders/schemas'
+import { Button } from '../ui/button'
+import { Icon } from '../icon'
+import { ArrowDownToLine } from 'lucide-react'
+import { Loader } from '../Loader'
 
-type EventListProps = {
+type EventListProps = LoadMoreProps & {
   events: Event[]
 }
 
-export const EventList = ({ events }: EventListProps) => {
+export const EventList = ({ events, ...loadMoreProps }: EventListProps) => {
   const { mutate: deleteEvent } = useDeleteEventMutation()
 
   return (
@@ -18,6 +22,30 @@ export const EventList = ({ events }: EventListProps) => {
           onDelete={() => deleteEvent(event.id)}
         />
       ))}
+
+      <LoadMore {...loadMoreProps} />
     </div>
+  )
+}
+
+type LoadMoreProps = {
+  fetchNextPage: (() => void) | undefined
+  isFetching: boolean
+}
+
+const LoadMore = ({ fetchNextPage, isFetching }: LoadMoreProps) => {
+  if (!fetchNextPage) return null
+
+  if (isFetching) return <Loader />
+
+  return (
+    <Button
+      size='icon'
+      variant='outline'
+      className='mx-auto rounded-full'
+      onClick={fetchNextPage}
+    >
+      <Icon icon={ArrowDownToLine} />
+    </Button>
   )
 }
--- apps/client/src/routes/_layout/events.tsx
+++ apps/client/src/routes/_layout/events.tsx
@@ -4,19 +4,26 @@ import { useAutoAnimate } from '@formkit/auto-animate/react'
 import { Loader } from '@/components/Loader'
 import { EventList } from '@/components/events/event-list'
 import { Controls } from '@/components/events/controls'
+import { last } from 'remeda'
 
 const EventsList = () => {
   const [parent] = useAutoAnimate()
 
   const {
     data: { pages },
+    fetchNextPage,
+    isFetching,
   } = useEventsQuery()
 
   return (
     <main ref={parent} className='flex grow flex-col pr-0 leading-none'>
       <Controls />
 
-      <EventList events={pages.flatMap((p) => p.data)} />
+      <EventList
+        events={pages.flatMap((p) => p.data)}
+        fetchNextPage={last(pages)?.nextOffset ? fetchNextPage : undefined}
+        isFetching={isFetching}
+      />
     </main>
   )
 }
